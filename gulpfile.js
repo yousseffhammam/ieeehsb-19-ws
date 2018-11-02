1de8f689fd2a822a801b485c4deae12b6fe6851b
Mohamed Emad <mohamed-emad@start-solutions.net>
2018-11-02 18:55:42 +0200

    add menu to website

--- gulpfile.js
+++ gulpfile.js
@@ -8,69 +8,67 @@ const gulp = require("gulp"),
     imagemin = require('gulp-imagemin');
 	log = util.log;
 
-// compiling sass to css
 
+// moveHTML
+gulp.task('moveindex', () => {
+    gulp.src('*.html')
+    .pipe(gulp.dest('./build/'))
+});
+// compiling sass to css
 gulp.task("styles", function(){
 	log("Generate CSS files " + (new Date()).toString());
     gulp.src("./sass/**/*.scss")
-		.pipe(sass({
-            style: 'expanded',
-            includePaths: ['node_modules/susy/sass/']
-        }))
-        .pipe(autoprefixer("last 3 version","safari 5", "ie 8", "ie 9"))
-		.pipe(gulp.dest("./css"))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(minifycss())
-		.pipe(gulp.dest('./css'));
+    .pipe(sass({
+        style: 'expanded',
+        includePaths: ['node_modules/susy/sass/']
+    }))
+    .pipe(autoprefixer("last 3 version","safari 5", "ie 8", "ie 9"))
+    .pipe(gulp.dest("./build/css"))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('./build/css'));
 });
 
 
 
 // minifying js script
 gulp.task('scripts', () => {
-    gulp.src([
-            'js/*.js'
-        ])
-        .pipe(uglify())
-        .pipe(gulp.dest('./js'));
+    gulp.src(['js/*.js'])
+    // .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
 });
 
 // compress images
 gulp.task('image', () => {
-    gulp.src('img/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./images'));
+    gulp.src('images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'));
 });
 
-// font awesome
-gulp.task('fontawesome', () => {
+// font awesome style
+gulp.task('fontawesomeCSS', () => {
     gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-        .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./build/css'));
 });
 
-// fonts
-gulp.task('fonts', () => {
+
+// font awesome 
+gulp.task('fontawesomeFonts', () => {
     gulp.src('node_modules/font-awesome/fonts/*')
-        .pipe(gulp.dest('./fonts'))
+    .pipe(gulp.dest('./build/fonts'))
 });
 
 // watching files
 gulp.task('watch', () => {
+    gulp.watch('*.html', ["moveindex"]);
     gulp.watch([
         'node_modules/susy/sass/_susy.scss',
         'sass/**/*.scss'
     ], ['styles']);
     gulp.watch('js/*.js', ['scripts']);
-    gulp.watch('img/*', ['image']);
+    gulp.watch('images/*', ['image']);
 });
 
 
 
-gulp.task('default', [
-    'styles',
-    'scripts',
-    'image',
-    'fontawesome',
-    'fonts',
-    'watch'
-])
+gulp.task('default', ['moveindex','styles','scripts','image','fontawesomeCSS','fontawesomeFonts','watch']);
