4d6d5259d8b6eca75b0e41945d04ecd1ba39e1c5
Mohamed Emad <m.emad550@gmail.com>
2018-11-21 22:58:21 +0200

    fix UI issues in home page

--- js/menu-toggle.js
+++ js/menu-toggle.js
@@ -1,32 +1,31 @@
-let menu_button = document.getElementById('menu-toggle'),
-    dash_menu_button = document.getElementById('dash-menu-toggle'),
-    aside = document.querySelector('aside'),
-    virtical_Menu = document.getElementById('virtical_menu');
+// let menu_button = document.getElementById('menu-toggle'),
+//     dash_menu_button = document.getElementById('dash-menu-toggle'),
+//     aside = document.querySelector('aside'),
+//     virtical_Menu = document.getElementById('virtical_menu'),
+let   header = document.querySelector('header');
+// dash_menu_button.addEventListener('click', function () {
+//     if (aside.style.display == "none") {
+//         aside.style.display = "block";
+//     } else {
+//         aside.style.display = "none";
+//     }
+// });
 
-dash_menu_button.addEventListener('click', function () {
-    if (aside.style.display == "none") {
-        aside.style.display = "block";
+// menu_button.addEventListener('click', function () {
+//     if (virtical_Menu.style.display == "none") {
+//         virtical_Menu.style.display = "block";
+//     } else {
+//         virtical_Menu.style.display = "none";
+//     }
+// });
+
+
+
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset >= 200){
+        header.classList.add('sticky');
     } else {
-        aside.style.display = "none";
+        header.classList.remove('sticky');
     }
-});
-
-menu_button.addEventListener('click', function () {
-    if (virtical_Menu.style.display == "none") {
-        virtical_Menu.style.display = "block";
-    } else {
-        virtical_Menu.style.display = "none";
-    }
-});
-
-
-
-$(window).scroll(function () {
-    var scrollPos = $(window).scrollTop();
-
-    if (scrollPos > 100) {
-        $('header').addClass('sticky');
-    } else {
-        $('header').removeClass('sticky');
-    }
+    
 });
